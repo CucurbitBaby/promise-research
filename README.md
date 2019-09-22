90ca796582c59ac17c3f90e5aabf19aa20d02d7b
cucurbitbaby <cucurbitbaby@163.com>
2019-09-23 03:27:37 +0800

    optimize the code, 'this' pointing to the problem

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 # 原生Promise new Promise时 参数传递错误
 node index.js
 
-# 自定义Promise中的使用箭头函数
+# 自定义Promise优化代码
+# 提取方法之后的this指向问题
 node test.js
 
 
--- promise.js
+++ promise.js
@@ -5,28 +5,30 @@ class Promise {
       throw new TypeError(`Promise resolver ${executor} is not function`)
     }
 
-    // 初始化值
-    this.value = null         // 终值
-    this.reason = null        // 拒因
-    this.state = 'pending'    // 状态  
+    this.initValue()
 
 
-    const resolve = value => {        // 成功后的一系列操作(状态的改变,成功回调的执行)
-      
-      if(this.state === 'pending') {  // 不可逆
-        this.state = 'fulfilled'      // state change
-        this.value = value            // resolve callback
-      }
+    executor(this.resolve, this.reject)
+  }
+
+  initValue() {                 // 初始化值
+    this.value  = null          // 终值
+    this.reason = null          // 拒因
+    this.state  = 'pending'     // 状态  
+  }
+
+  resolve(value) {                  // 成功后的一系列操作(状态的改变,成功回调的执行)
+    if(this.state === 'pending') {  // 不可逆
+      this.state = 'fulfilled'      // state change
+      this.value = value            // resolve callback
     }
-      
-    const reject = reason => {        // 失败后的一系列操作(状态的改变,失败回调的执行)
-      if(this.state === 'pending') {  // 不可逆
-        this.state = 'rejected'       // state change
-        this.reason = reason          // reject callback
-      }
-    }
+  }
 
-    executor(resolve, reject)
+  reject(reason) {                   // 失败后的一系列操作(状态的改变,失败回调的执行)
+    if(this.state === 'pending') {   // 不可逆
+      this.state  = 'rejected'       // state change
+      this.reason = reason           // reject callback
+    }
   }
 }
 
