fe25d6c00f67d033e60a98a8e643510c796368e4
cucurbitbaby <cucurbitbaby@163.com>
2019-09-23 03:50:34 +0800

    continue to optimize,add const

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 node index.js
 
 # 添加then方法，判断params是否时函数，不完全忽略
+# 继续优化，添加常量
 node test.js
 
 
--- promise.js
+++ promise.js
@@ -16,23 +16,23 @@ class Promise {
     this.reject   = this.reject.bind(this)
   }
 
-  initValue() {                 // 初始化值
-    this.value  = null          // 终值
-    this.reason = null          // 拒因
-    this.state  = 'pending'     // 状态  
+  initValue() {                       // 初始化值
+    this.value  = null                // 终值
+    this.reason = null                // 拒因
+    this.state  = Promise.PENDING     // 状态  
   }
 
-  resolve(value) {                  // 成功后的一系列操作(状态的改变,成功回调的执行)
-    if(this.state === 'pending') {  // 不可逆
-      this.state = 'fulfilled'      // state change
-      this.value = value            // resolve callback
+  resolve(value) {                          // 成功后的一系列操作(状态的改变,成功回调的执行)
+    if(this.state === Promise.PENDING) {    // 不可逆
+      this.state = Promise.FULFILLED        // state change
+      this.value = value                    // resolve callback
     }
   }
 
-  reject(reason) {                   // 失败后的一系列操作(状态的改变,失败回调的执行)
-    if(this.state === 'pending') {   // 不可逆
-      this.state  = 'rejected'       // state change
-      this.reason = reason           // reject callback
+  reject(reason) {                         // 失败后的一系列操作(状态的改变,失败回调的执行)
+    if(this.state === Promise.PENDING) {   // 不可逆
+      this.state  = Promise.REJECTED       // state change
+      this.reason = reason                 // reject callback
     }
   }
 
@@ -49,14 +49,18 @@ class Promise {
       }
     }
 
-    if(this.state === 'fulfilled') {
+    if(this.state === Promise.FULFILLED) {
       onFulfilled(this.value)
     }
     
-    if(this.state === 'rejected') {
+    if(this.state === Promise.REJECTED) {
       onRejected(this.reason)
     }
   }
 }
+
+Promise.PENDING   = 'pending'
+Promise.FULFILLED = 'fulfilled'
+Promise.REJECTED  = 'rejected'
 
 module.exports = Promise
